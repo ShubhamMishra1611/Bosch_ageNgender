9c4ed303578eafa67750d9c8a870643139b7fa00
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-12-19 19:49:55 +0530

    removed incorrect condition and debug statements

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from age_and_gender import detect_age_and_gender
  
 capture = cv2.VideoCapture('video.mp4')
  
-fr_no=0
+
 while (True):
 
     success, frame = capture.read()
     print(success)
-    if success and fr_no %100 ==0 : 
+    if success : 
         sr = cv2.dnn_superres.DnnSuperResImpl_create()
         path = 'models/EDSR_x4.pb'
         sr.readModel(path)
         sr.setModel("edsr",4)
         result = sr.upsample(frame)
-        print("done")
+        
         newresult = detect_age_and_gender(result)
         cv2.imwrite(f'image_{fr_no}.jpg',result)
         fr_no=fr_no+1        
